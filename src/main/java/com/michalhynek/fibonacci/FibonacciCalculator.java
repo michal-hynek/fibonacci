aff4aa3b083ad4433c251e6da6b9fa812b167fef
Michal Hynek <michal.hynek@allocadia.com>
2019-05-04 07:05:17 -0700

    Updated FibonacciCalculator to handle large inputs
    I used the dynamic programming technique to make sure Fibonacci numbers are not needlessly recalculated.  It's basically just a map that caches already calculated number.

--- src/main/java/com/michalhynek/fibonacci/FibonacciCalculator.java
+++ src/main/java/com/michalhynek/fibonacci/FibonacciCalculator.java
@@ -1,14 +1,29 @@
 package main.java.com.michalhynek.fibonacci;
 
+import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.Map;
+
 public class FibonacciCalculator {
 
-    public static int calculate(int n) {
+    private static final Map<Integer, BigInteger> cache = new HashMap<>();
+
+    public static BigInteger calculate(int n) {
         if (n == 0) {
-            return 0;
+            return BigInteger.ZERO;
         } else if (n == 1) {
-            return 1;
+            return BigInteger.ONE;
+        } else if (cache.containsKey(n)) {
+            return cache.get(n);
         } else {
-            return calculate(n - 1) + calculate(n - 2);
+            BigInteger fibNMinus1 = calculate(n - 1);
+            BigInteger fibNMinus2 = calculate(n - 2);
+
+            BigInteger result = fibNMinus1.add(fibNMinus2);
+
+            cache.put(n, result);
+
+            return result;
         }
     }
 }
--- src/test/java/com/michalhynek/fibonacci/FibonacciCalculatorTest.java
+++ src/test/java/com/michalhynek/fibonacci/FibonacciCalculatorTest.java
@@ -6,24 +6,36 @@ import main.java.com.michalhynek.fibonacci.FibonacciCalculator;
 
 import org.junit.Test;
 
+import java.math.BigInteger;
+
 public class FibonacciCalculatorTest {
 
     @Test
-    public void testCalculate_forN0() {
-        assertEquals(0, FibonacciCalculator.calculate(0));
+    public void testCalculate_n0() {
+        assertEquals(BigInteger.ZERO, FibonacciCalculator.calculate(0));
     }
 
     @Test
-    public void testCalculate_forN1() {
-        assertEquals(1, FibonacciCalculator.calculate(1));
+    public void testCalculate_n1() {
+        assertEquals(BigInteger.ONE, FibonacciCalculator.calculate(1));
     }
 
     @Test
-    public void testCalculate_forSmallNs() {
-        assertEquals(1, FibonacciCalculator.calculate(2));
-        assertEquals(2, FibonacciCalculator.calculate(3));
-        assertEquals(3, FibonacciCalculator.calculate(4));
-        assertEquals(5, FibonacciCalculator.calculate(5));
-        assertEquals(8, FibonacciCalculator.calculate(6));
+    public void testCalculate_smallNs() {
+        assertEquals(BigInteger.valueOf(1), FibonacciCalculator.calculate(2));
+        assertEquals(BigInteger.valueOf(2), FibonacciCalculator.calculate(3));
+        assertEquals(BigInteger.valueOf(3), FibonacciCalculator.calculate(4));
+        assertEquals(BigInteger.valueOf(5), FibonacciCalculator.calculate(5));
+        assertEquals(BigInteger.valueOf(8), FibonacciCalculator.calculate(6));
+    }
+
+    @Test
+    public void testCalcute_largeNs() {
+        assertEquals(new BigInteger("190392490709135"), FibonacciCalculator.calculate(70));
+        assertEquals(new BigInteger("354224848179261915075"), FibonacciCalculator.calculate(100));
+        assertEquals(
+            new BigInteger("222232244629420445529739893461909967206666939096499764990979600"),
+            FibonacciCalculator.calculate(300)
+        );
     }
 }
