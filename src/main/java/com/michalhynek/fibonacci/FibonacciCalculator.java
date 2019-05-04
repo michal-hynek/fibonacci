88e608872d3af77dfbbb959314738ae512d93b55
Michal Hynek <michal.hynek@allocadia.com>
2019-05-04 07:21:33 -0700

    Optimized FibonacciCalculator so it doesn't use cache
    This is basically a tail recursive implementation of Fibonacci calculator.  Java doesn't support tail recursion, so there's still a StackOverflowException for large numbers (> 100,000)

--- src/main/java/com/michalhynek/fibonacci/FibonacciCalculator.java
+++ src/main/java/com/michalhynek/fibonacci/FibonacciCalculator.java
@@ -1,29 +1,20 @@
 package main.java.com.michalhynek.fibonacci;
 
 import java.math.BigInteger;
-import java.util.HashMap;
-import java.util.Map;
 
 public class FibonacciCalculator {
 
-    private static final Map<Integer, BigInteger> cache = new HashMap<>();
-
     public static BigInteger calculate(int n) {
+        return calculate(n, BigInteger.ZERO, BigInteger.ONE);
+    }
+
+    private static BigInteger calculate(int n, BigInteger nMinus2, BigInteger nMinus1) {
         if (n == 0) {
-            return BigInteger.ZERO;
+            return nMinus2;
         } else if (n == 1) {
-            return BigInteger.ONE;
-        } else if (cache.containsKey(n)) {
-            return cache.get(n);
+            return nMinus1;
         } else {
-            BigInteger fibNMinus1 = calculate(n - 1);
-            BigInteger fibNMinus2 = calculate(n - 2);
-
-            BigInteger result = fibNMinus1.add(fibNMinus2);
-
-            cache.put(n, result);
-
-            return result;
+            return calculate(n - 1, nMinus1, nMinus1.add(nMinus2));
         }
     }
 }
